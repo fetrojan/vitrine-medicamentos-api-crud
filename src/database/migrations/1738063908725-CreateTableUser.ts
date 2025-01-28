db0483838011351be9eff66afeb71c232ffab493
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2025-01-28 08:35:13 -0300

    feat:criaçao de entidade e migration User

---
+++ src/database/migrations/1738063908725-CreateTableUser.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTableUser1738063908725 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'int',
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'email',
+                    type: 'varchar'
+                },
+                {
+                    name: 'senha',
+                    type: 'varchar'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users')
+    }
+
+}
