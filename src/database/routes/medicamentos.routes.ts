4cb3bc654b0dc392b888afa431f58a19df47af87
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2025-01-28 11:43:24 -0300

    feat criada a paginaçao para os medicamentos

--- src/database/routes/medicamentos.routes.ts
+++ src/database/routes/medicamentos.routes.ts
@@ -33,10 +33,17 @@ medicamentoRouter.get('/', async (req: Request, res: Response) => {
             return
         }
 
+        const page = Number(req.query.page ?? 1)
+        const limit = Number(req.query.limit ?? 10)
+
+        const skip = page > 1 ? (page - 1) * limit : 0
+
         const result = await medicamentoRepository.find({
             where: {
                 userId: userId
-            }
+            },
+            skip: skip,
+            take: limit
         });
 
         if(!result) {
@@ -76,8 +83,15 @@ medicamentoRouter.get('/id', async (req: Request, res: Response) => {
 
 medicamentoRouter.get('/all', async (req: Request, res: Response) => {
     try {
+        const page = Number(req.query.page ?? 1)
+        const limit = Number(req.query.limit ?? 10)
 
-        const result = await medicamentoRepository.find();
+        const skip = page > 1 ? (page - 1) * limit : 0
+
+        const result = await medicamentoRepository.find({
+            skip: skip,
+            take: limit
+        });
 
         if(!result) {
             res.status(404).json("Nenhum medicamento encontrado!")
